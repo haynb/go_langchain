3a1c41b5b40a37f15ae97c794f893034dd8ea4cf
haynb <2587971441@qq.com>
2023-08-22 15:16:10 +0800

    v0.1

---
+++ go.mod
@@ -0,0 +1,11 @@
+module kefu
+
+go 1.20
+
+require github.com/tmc/langchaingo v0.0.0-20230818002435-fef08210d974
+
+require (
+	github.com/dlclark/regexp2 v1.8.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/pkoukk/tiktoken-go v0.1.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/llms/openai"
+	"github.com/tmc/langchaingo/schema"
+	"os"
+)
+
+func main() {
+	if err := os.Setenv("OPENAI_API_KEY", "sk-UhzxW8avagN8D00Q2AmHeJL1LBz6NFv9rI3USa94GiXGPa9r"); err != nil {
+		panic(err)
+	}
+
+	if err := os.Setenv("OPENAI_BASE_URL", "https://cfwus02.opapi.win/v1/"); err != nil {
+		panic(err)
+	}
+	if err := os.Setenv("OPENAI_MODEL", "gpt-3.5-turbo"); err != nil {
+		panic(err)
+	}
+	llm, err := openai.NewChat()
+	if err != nil {
+		panic(err)
+	}
+	completion, err := llm.Call(context.Background(), []schema.ChatMessage{
+		//schema.SystemChatMessage{Content: "Hello, I am a friendly chatbot. I love to talk about movies, books and music. Answer in long form yaml."},
+		schema.HumanChatMessage{Content: "鲁迅为什么要打周树人?"},
+	}, llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
+		fmt.Println("chunk: ", string(chunk)) // 流式输出
+		return nil
+	}),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("completion：", completion)
+}
