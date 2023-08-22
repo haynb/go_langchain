67b50774b2d278e021dfed9bdd262da752c828d7
haynb <2587971441@qq.com>
2023-08-22 16:25:02 +0800

    doc

--- main.go
+++ main.go
@@ -3,38 +3,56 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/chains"
+	"github.com/tmc/langchaingo/documentloaders"
 	"github.com/tmc/langchaingo/llms/openai"
-	"github.com/tmc/langchaingo/schema"
+	"github.com/tmc/langchaingo/textsplitter"
 	"os"
 )
 
 func main() {
-	if err := os.Setenv("OPENAI_API_KEY", "sk-UhzxW8avagN8D00Q2AmHeJL1LBz6NFv9rI3USa94GiXGPa9r"); err != nil {
-		panic(err)
+	// 定义要设置的环境变量和对应的值
+	envVars := map[string]string{
+		"OPENAI_API_KEY":  "sk-UhzxW8avagN8D00Q2AmHeJL1LBz6NFv9rI3USa94GiXGPa9r",
+		"OPENAI_BASE_URL": "https://cfwus02.opapi.win/v1/",
+		"OPENAI_MODEL":    "gpt-3.5-turbo",
 	}
 
-	if err := os.Setenv("OPENAI_BASE_URL", "https://cfwus02.opapi.win/v1/"); err != nil {
-		panic(err)
-	}
-	if err := os.Setenv("OPENAI_MODEL", "gpt-3.5-turbo"); err != nil {
-		panic(err)
+	// 逐个设置环境变量
+	for key, value := range envVars {
+		err := os.Setenv(key, value)
+		if err != nil {
+			panic(err)
+		}
 	}
 	llm, err := openai.NewChat()
 	if err != nil {
 		panic(err)
 	}
-	completion, err := llm.Call(context.Background(), []schema.ChatMessage{
-		//schema.SystemChatMessage{Content: "Hello, I am a friendly chatbot. I love to talk about movies, books and music. Answer in long form yaml."},
-		schema.HumanChatMessage{Content: "鲁迅为什么要打周树人?"},
-	}, llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
-		fmt.Println("chunk: ", string(chunk)) // 流式输出
-		return nil
-	}),
-	)
+	stuffQAChain := chains.LoadStuffQA(llm)
+	file, err := os.Open("xiaozhao.pdf")
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Println("completion：", completion)
+	// 获取文件大小
+	fileInfo, err := file.Stat()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fileSize := fileInfo.Size()
+	p := documentloaders.NewPDF(file, fileSize)
+	spliter := textsplitter.NewTokenSplitter()
+	doc, err := p.LoadAndSplit(context.Background(), spliter)
+	if err != nil {
+		panic(err)
+	}
+	answer, err := chains.Call(context.Background(), stuffQAChain, map[string]any{
+		"input_documents": doc,
+		"question":        "住宿费多少钱?",
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print(answer)
 }
